07440eb5c9ca85390210eb2f41d08cbbc0c01b64
jmarroayve-compsci <jmarroayve.compsci@gmail.com>
2021-09-10 15:36:11 -0400

    refactoring, typescript, lint

--- scripts/generate_docs.ps1
+++ scripts/generate_docs.ps1
@@ -1,5 +1,5 @@
 
 
 redoc-cli bundle ../files/api.yaml --options.theme.colors.primary.main=orange
-rm ../docs/index.html
-mv ./redoc-static.html ../docs/index.html
+rm ../src/files/docs/index.html
+mv ./redoc-static.html ../src/files/docs/index.html
