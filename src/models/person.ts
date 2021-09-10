fe90ee6f60ec586a2876847fdf1663d33be7817b
jmarroayve-compsci <jmarroayve.compsci@gmail.com>
2021-09-10 16:04:53 -0400

    refactoring, typescript, lint

--- src/models/person.ts
+++ src/models/person.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Person = new mongoose.Schema({
 	name: String,
 	directed: [ { type: mongoose.Schema.Types.ObjectId, ref: 'movie' } ],
-	acted: [ { type: mongoose.Schema.Types.ObjectId, ref: 'movie' },
+	acted: [ { type: mongoose.Schema.Types.ObjectId, ref: 'movie' } ],
 }, { collection: 'person' })
 
 export default mongoose.model("person", Person);
