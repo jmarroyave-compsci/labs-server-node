1da9631a01eaa1323200553d811c7e0725b44a69
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2021-09-28 20:26:14 -0400

    settings for CI/CD

--- src/controllers/2.0/Data.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import * as Service from '../../services/StoriesService';
-import * as utils from '../../lib/misc';
-import { default as P } from "bluebird";
-
-export async function oscarsGet(req: Request, res: Response): P<any> {
-  const data = await Service.oscarsGet( {} );
-  utils.writeJSON(res, data);
-};
