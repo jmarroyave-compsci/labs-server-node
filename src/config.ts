5f1f24502e01cc1e9902b33ad0fbe178fac71d8a
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2021-10-13 20:11:36 -0400

    changing repo name

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const config = {
   PORT: 8080,
   DB_SERVER: "mongodb://127.0.0.1:27017/entertainment",
   WEB_SERVER: "http://localhost:8080/",
-  VERSION: "0.6.6",
+  VERSION: "0.6.13",
 }
 
 export default config;
