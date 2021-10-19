d4c6c03a5f23ec3efd8bae00dde56d150cb55d3d
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2021-10-19 15:50:33 -0400

    search

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const config = {
   PORT: 8080,
   DB_SERVER: "mongodb://127.0.0.1:27017/ent_dev",
   WEB_SERVER: "http://localhost:8080/",
-  VERSION: "1.1.2",
+  VERSION: "1.1.2.2",
   CACHE_SERVER: false,
 }
 
