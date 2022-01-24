f4e981a67aaa38bf30dc8ce4d5c768ec2ff40632
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2022-01-24 17:55:31 -0500

    v.4.0

--- src/api/4.0/routes/graphql.ts
+++ src/api/4.0/routes/graphql.ts
@@ -7,7 +7,7 @@ import express from "express";
 const router = express.Router();
 
 ( async () => {
-	const oasFile = await getSpecs( { version : "3.0"} );
+	const oasFile = await getSpecs( { version : "4.0"} );
 	const oasDoc = jsyaml.safeLoad(oasFile);
 	const {schema} = await createGraphQLSchema(oasDoc, { 
 		simpleNames: true,
