a8aed608b834804593dddb087da202c0d123541d
jmarroyave-compsci <jmarroyave.compsci@gmail.com>
2022-03-07 16:00:33 -0500

    fix search page

--- src/api/4.0/controllers/Search.ts
+++ src/api/4.0/controllers/Search.ts
@@ -5,12 +5,12 @@ import * as utils from 'lib/misc';
 import { default as P } from "bluebird";
 
 export async function searchResultsGet(req: Request, res: Response): P<any> {
-  const params = { qry: "", entities: [] }
+  const params = { qry: "", entities: [], page: null }
   params.qry = decodeURIComponent(req?.query?.qry.toString())
   params['entities'] = JSON.parse(decodeURIComponent(req?.query?.entities?.toString()))
+  params.page = (req?.query?.page) ? req?.query?.page : 1;
   HistoryService.addSearched(req, params.qry)
-  
-  
+   
   const data = await Service.searchResults( params );
   utils.writeJSON(res, data);
 };
