3c2c30f3e14d0dfc0806bd0c38cb1cb389449ffb
jmarroyave.compsci <jmarroyave.compsci@gmail.com>
2022-11-22 09:21:58 -0600

    loaders

---
+++ src/server/loaders/db/mongo.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import { getServices, loadClass, classExists } from 'common/files'
+import config from 'common/config'
+import log from 'common/log';
+import { Connection } from 'common/db'
+
+export async function loadDBMongo(){
+  const conns = []
+
+  for(const service of getServices()){
+    const configClass = `${service.versionPath}/config/`
+    if( !classExists( configClass ) ) continue
+
+    const { DB } = await loadClass(configClass)
+    conns.push(
+      [ DB.server, DB.database, service.version ]
+    ) 
+  }
+
+  log.info("INIT CONNECTIONS")
+
+  for(const conn of conns){
+    await connect(conn[0], conn[1], conn[2])
+  }
+
+  log.info(`CONNECTIONS INITIALIZED ${Object.keys(Connection.CONNECTIONS)}`)
+}
+
+async function connect( server, dbname, version ){
+    const key = `${server}/${dbname}/${version}`
+    try{
+        log.info(` DB: CONNECTING TO DB SERVER [${server}] DB [${dbname}]`)
+        server = config.DB.SERVERS[server]
+        const connString = `mongodb+srv://${server}/${dbname}?retryWrites=true&w=majority`
+        //log.info(connString)
+        const mconn = await mongooseCreateConnection(connString);
+        //log.info(mconn.readyState)
+        Connection.CONNECTIONS[key] = mconn
+        log.info(" DB: CONNECTION SUCCESFULL")
+        log.info("-".repeat(50))
+      } catch( ex ){
+        log.error(ex.toString())
+      }
+}
+
+async function mongooseCreateConnection( connString ){
+  const mconn = await mongoose.createConnection(connString, {
+    bufferCommands: false,
+    autoIndex: false,
+  }).asPromise();
+  return mconn
+}
+
+async function mongooseConnect( connString ){
+  return new Promise( ( resolve, reject ) => {
+    mongoose.connect(connString, {
+      bufferCommands: false,
+      autoIndex: false,
+    }).then( resp => resolve( mongoose ) )
+    .catch( ex => reject(ex) );
+  })
+}
---
+++ src/server/loaders/graphql.ts
@@ -0,0 +1,51 @@
+import * as fs from 'fs'
+import log from 'common/log';
+import express from "express";
+import { graphqlHTTP }  from 'express-graphql'
+import { createGraphQLSchema } from 'openapi-to-graphql'
+import { getSpecs } from "common/docs";
+import jsyaml from 'js-yaml';
+import { getServices, loadClass, classExists } from 'common/files'
+
+export const loadGraphQL = async function( app ){
+    log.info("LOADING GRAPHQL ROUTES")
+    const router = express.Router();
+
+    const loadSpec = async function( service ) {
+        const oasFile = await getSpecs( service.name, service.version );
+        if(!oasFile) {
+            log.info(`     SKIPPING: SPECS FILE NOT FOUND`);
+            return
+        }
+
+        const oasDoc = jsyaml.safeLoad(oasFile);
+        const {schema} = await createGraphQLSchema(oasDoc, { 
+                simpleNames: true,
+                operationIdFieldNames : true,
+                headers: function(a,aa,c,x) {
+                    const b = x['context']
+                    return { cookie: b['headers']['cookie'] }
+                },          
+            }
+        )
+
+        const routeEndpoint = `/${service.name}/${service.version}/graphql`
+        log.info(`     - ${routeEndpoint}`);
+        router.use(routeEndpoint, graphqlHTTP( (req, res) => {
+            return {
+                schema, 
+                context: req,
+                graphiql: true, 
+                pretty: true
+            }
+        }));
+    }
+
+
+    for( const service of getServices() ){
+        log.info(`  SERVICE: ${service.name} v.${service.version}`);
+        await loadSpec(service)
+    }
+
+    app.use('/', router);
+}
