2956587b3c252bfd2f64b86f3c259bed0d1fe260
jmarroyave.compsci <jmarroyave.compsci@gmail.com>
2023-01-22 01:02:37 -0600

    auth

--- src/server/middleware/session/session.express-session.ts
+++ src/server/middleware/session/session.express-session.ts
@@ -8,7 +8,7 @@ export const init = ( app ) => {
     saveUninitialized: true,
     resave: false,
     cookie: {
-      sameSite: false,
+      sameSite: 'none',
       secure: !CONFIG.LOCAL,
       maxAge: 1000 * 60 * 60 * 24,
       httpOnly: false,
