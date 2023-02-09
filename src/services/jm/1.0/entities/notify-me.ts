42fa5a674aa6ac3951b2f2ff08505bc91b2632d7
jmarroyave.compsci <jmarroyave.compsci@gmail.com>
2023-02-09 10:44:57 -0600

    JM: notifyMe

---
+++ src/services/jm/1.0/entities/notify-me.ts
@@ -0,0 +1,23 @@
+import CONFIG from 'common/config'
+import { invoke } from 'common/service'
+
+export async function send( query, params, session ) {
+  const parameters = {
+    to: CONFIG.SERVICES.JM.NOTIFY_MAIL,
+    from: CONFIG.PLUGINS.SENDGRID.SENDER,
+    subject: params.subject,
+    bodyText: params.body,
+  }
+
+  //console.log("NOTIFY ME", parameters)
+
+  await invoke({
+    service: 'communication',
+    version: '1.0',
+    entity: 'mail',
+    operation: 'send',
+    params: parameters,
+    session: session,
+  })
+}
+
