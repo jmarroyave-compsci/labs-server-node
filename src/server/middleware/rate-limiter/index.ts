68dbe274a5630a12f77ecdfd0f21f7a5f25bcebe
jmarroyave.compsci <jmarroyave.compsci@gmail.com>
2023-02-09 12:30:26 -0600

    middleware: limit

---
+++ src/server/middleware/rate-limiter/index.ts
@@ -0,0 +1,31 @@
+//const { RateLimiterMemory } = require('rate-limiter-flexible');
+
+//npm i --save rate-limiter-flexible
+
+export const init = ( app ) => {
+    app.use(middleware)
+}
+
+const middleware = function(req, res, next) {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+  const key = ip
+  const pointsToConsume = 1;
+
+  /*
+  const rateLimiter = new RateLimiterMemory({
+    points: 20,
+    duration: 1,
+  });
+  rateLimiter.consume(key, pointsToConsume) 
+    .then((rateLimiterRes) => {
+  */
+      next();
+  /*
+    })
+    .catch((rej) => {
+      res.status(429).send('Too Many Requests');
+    });   
+  */
+};
+
+
